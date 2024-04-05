f860ca212824deffc005ec93e115f91f82baba2b
Lalu Immaratul Ardhi Ganeru <immaratulganeru@gmail.com>
2024-04-05 16:11:41 +0700

    Jobsheet 7 stok

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\POSController;
+use App\Http\Controllers\StokController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 
@@ -65,6 +66,16 @@ Route::group(['prefix' => 'barang'], function() {
     Route::delete('/{id}', [BarangController::class, 'destroy']); // menghapus data user
 });
 
+Route::group(['prefix' => 'stok'], function() {
+    Route::get('/', [StokController::class, 'index']);          // menampilkan halaman awal user
+    Route::post('/list', [StokController::class, 'list']);      // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [StokController::class, 'create']);   // menampilkan halaman form tambah user
+    Route::post('/', [StokController::class, 'store']);         // menyimpan data user baru
+    Route::get('/{id}', [StokController::class, 'show']);       // menampilkan detail user
+    Route::get('/{id}/edit', [StokController::class, 'edit']);  // menampilkan halaman form edit user
+    Route::put('/{id}', [StokController::class, 'update']);     // menyimpan perubahan data user
+    Route::delete('/{id}', [StokController::class, 'destroy']); // menghapus data user
+});
 // Route::get('/level', [LevelController::class, 'index']);
 // Route::get('/kategori', [KategoriController::class, 'index']);
 // Route::get('/user', [UserController::class, 'index']);
