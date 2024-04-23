af16d9df1dbbe9b33dd96696bc2df0444cb7cda5
Lalu Immaratul Ardhi Ganeru <immaratulganeru@gmail.com>
2024-04-23 10:07:06 +0700

    Jobsheet 10 praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,6 +10,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class UserModel extends Authenticatable
 {
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
+    
     use HasFactory;
 
     protected $table = 'm_user';
