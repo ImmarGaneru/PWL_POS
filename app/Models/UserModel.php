0b02bed750ac1987e40ca403af734b2abdf9a7b3
Lalu Immaratul Ardhi Ganeru <immaratulganeru@gmail.com>
2024-05-06 20:12:24 +0700

    Jobsheet 11 praktikum

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
@@ -25,10 +26,22 @@ class UserModel extends Authenticatable implements JWTSubject
 
     //@var array
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = [
+        'level_id', 
+        'username', 
+        'nama', 
+        'password',
+        'image'
+    ];
 
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    protected function image(): Attribute{
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
     }
 }
